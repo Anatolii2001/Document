9e9c3cc430cfc2448f0f3309690dc1a928646fb0
Anatoliy Girak <agirak1@rambler.ru>
2022-09-14 13:47:10 +0300

    Update

---
+++ src/main/java/com/example/Spring_boot/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.example.Spring_boot.dao;
+
+import com.example.Spring_boot.model.User;
+import java.util.List;
+
+public interface UserDao {
+    List<User> getAllUsers();
+    void save(User user);
+    User findById(int id);
+    void update(int id, User updateUser);
+    void delete(int id);
+//    User isExistById(User user);
+}
---
+++ src/main/java/com/example/Spring_boot/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.Spring_boot.service;
+
+import com.example.Spring_boot.model.User;
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAllUsers();
+    void save(User user);
+    User findById(int id);
+    void update(int id, User updateUser);
+    void delete(int id);
+//    User isExistById(User user);
+}
